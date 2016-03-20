9ac3551712793baf307a4f083e7740560f9eb5ab
Maria Sindeeva <ilapsya@gmail.com>
2016-03-20 17:47:52 +0300

    correct app inclusion

--- sphere_web/settings.py
+++ sphere_web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'flat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
 ]
 
 MIDDLEWARE_CLASSES = [
