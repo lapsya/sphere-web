e27935af1fd9b0cf056e0ee3eb8f6fd1d72af325
Maria Sindeeva <ilapsya@gmail.com>
2016-03-20 17:29:39 +0300

    add db models

--- shop/models.py
+++ shop/models.py
@@ -2,4 +2,43 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    rating = models.FloatField()
+    amount = models.IntegerField()
+    description = models.TextField()
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    vendor = models.ForeignKey(
+        'Vendor',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    tag = models.ManyToManyField(
+        'Tag',
+    )
+
+    def __unicode__(self):
+        return 'Item ' + self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return 'Category ' + self.title
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return 'Tag ' + self.title
+
+class Vendor(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return 'Vendor ' + self.title
